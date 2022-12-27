be4005aa4f4ffeb99dcc0bd5afdd644195cbb053
Bruno Leonel <bruno_leonels@hotmail.com>
2022-12-27 01:22:56 -0300

    Server-side streaming

--- src/main/java/org/example/server/BankService.java
+++ src/main/java/org/example/server/BankService.java
@@ -1,9 +1,12 @@
 package org.example.server;
 
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.example.models.Balance;
+import org.example.models.Money;
 import org.example.models.BalanceCheckRequest;
 import org.example.models.BankServiceGrpc;
+import org.example.models.WithdrawRequest;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
@@ -14,6 +17,33 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .setAmount(BankData.getBalance(accountNumber))
                 .build();
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<org.example.models.Money> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        int amount = request.getAmount();
+        int balance = BankData.getBalance(accountNumber);
+
+        if (balance < amount) {
+            Status status = Status.FAILED_PRECONDITION.withDescription("Not enough money.");
+            responseObserver.onError(status.asRuntimeException());
+            return;
+        }
+
+        for (int i = 0; i < (amount / 10); i++) {
+            Money money = Money.newBuilder()
+                    .setValue(10)
+                    .build();
+            responseObserver.onNext(money);
+            BankData.deductBalance(accountNumber, 10);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         responseObserver.onCompleted();
     }
 }
